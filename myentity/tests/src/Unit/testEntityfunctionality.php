4b78c383b8301c6818c43d9c5f16ab1ec0125ce0
subhransu <subhransu.tunga@ingrammicro.com>
2020-08-28 21:40:07 +0530

    Added unit test case files

---
+++ myentity/tests/src/Unit/testEntityfunctionality.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\Tests\myentity\Unit;
+
+use Drupal\Tests\UnitTestCase;
+use Drupal\myentity\Form\Eventsmanager;
+
+
+use Drupal\Core\Form\FormStateInterface;
+
+class testEntityfunctionality extends UnitTestCase {
+	
+
+public function testTitleValidation()
+{
+	$titleTest = new Eventsmanager();
+	$titleVals = $titleTest->validate_title('abcd'); //positive Test case
+	$this->assertEquals($titleVals,true);
+	$titleVals2 = $titleTest->validate_title(''); //Negetive test case
+	$this->assertEquals($titleVals2,false);
+	$titleVals3 = $titleTest->validate_title('Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem Ipsumsum'); //Negetive test case check length 25 character
+	$this->assertEquals($titleVals3,false);
+}
+
+
+public function testvenueValidation()
+{
+	$titleTest = new Eventsmanager();
+	$titleVals = $titleTest->validate_venue('abcd'); //positive Test case
+	$this->assertEquals($titleVals,true);
+	$titleVals2 = $titleTest->validate_venue(''); //Negetive test case
+	$this->assertEquals($titleVals2,false);
+	$titleVals3 = $titleTest->validate_venue('Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsumsum IpsumLorem IpsumLorem IpsumL'); //Negetive test case
+	$this->assertEquals($titleVals3,false);
+}
+
+}
+?>
